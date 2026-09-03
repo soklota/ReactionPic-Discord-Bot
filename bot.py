eeb68ca575fceb9f4dbc6047e53511962c8a968b
Sydney Oklota <70277278+soklota@users.noreply.github.com>
2026-09-03 12:54:41 -0400

    remove guild token and guild sync method

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import random
-GUILD_ID = discord.Object(id=1529995986770989278)  # paste your actual number, no quotes
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -20,27 +19,12 @@ intents.message_content = True  # needed if your bot reads message text
 bot = commands.Bot(command_prefix="!", intents=intents) 
 
 
-@bot.event #guild only sync
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-    bot.tree.copy_global_to(guild=GUILD_ID)
-    synced = await bot.tree.sync(guild=GUILD_ID)
-    print(f"Synced {len(synced)} command(s) to guild")
-
 @bot.event #global sync
 async def on_ready():
     print(f"Logged in as {bot.user}")
     synced = await bot.tree.sync()
     print(f"Synced {len(synced)} command(s) globally")
 
-#/ping command will respond with "Pong!" when used in Discord chat
-@bot.tree.command(name="ping", description="Replies with Pong!")
-async def slash_ping(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong!")
-
-@bot.command()  # old style, triggered by !ping
-async def ping(ctx):
-    await ctx.send("Pong!")
 
 #slash commands
 @bot.tree.command(name="sad", description="Replies with a random Sad Image") 
